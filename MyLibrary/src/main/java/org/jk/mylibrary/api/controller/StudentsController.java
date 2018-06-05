55004dbc861e724baaf6e77e87cf6cf9f60ff1d1
rohithsaitech <rohithsai.crs@gmail.com>
2018-06-05 12:46:06 +0530

    Add Student Request Mapping URL

--- MyLibrary/src/main/java/org/jk/mylibrary/api/controller/StudentsController.java
+++ MyLibrary/src/main/java/org/jk/mylibrary/api/controller/StudentsController.java
@@ -4,12 +4,13 @@ import org.jk.mylibrary.api.model.ResponseVO;
 import org.jk.mylibrary.api.model.StudentsVO;
 import org.jk.mylibrary.api.service.StudentsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/student")
 public class StudentsController {
 
 	@Autowired
